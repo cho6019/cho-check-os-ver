15bd7a58cf5545062cd3d64c160e10ee8ba75752
cho6019 <j01040864205@gmail.com>
2025-01-14 15:17:24 +0900

    몰라

--- src/cho_check_os_ver/randomimport.py
+++ src/cho_check_os_ver/randomimport.py
@@ -13,28 +13,28 @@ def pick():
 
     match a:
         case 1:
-            from check_os_ver.hi import hi as hi1
             print("hi1")
+            from check_os_ver.hi import hi as hi1
             return(hi1())
         case 2:
-            from hj_check_os_version.hi import hi as hi2
             print("hi2")
+            from hj_check_os_version.hi import hi as hi2
             return(hi2())
         case 3:
-            from jacob_os_version_check.hi import hi as hi3
             print("hi3")
+            from jacob_os_version_check.hi import hi as hi3
             return(hi3())
         case 4:
-            from lucas_check_os_ver.hi import hi as hi4
             print("hi4")
+            from lucas_check_os_ver.hi import hi as hi4
             return(hi4())
         case 5:
-            from stundrg_check_os_ver.hi import hi as hi5
             print("hi5")
+            from stundrg_check_os_ver.hi import hi as hi5
             return(hi5())
         case 6:
-            from nunininu_check_os_ver.hi import hi as hi6
             print("hi6")
+            from nunininu_check_os_ver.hi import hi as hi6
             return(hi6())
     #elif a==7:
         #A=hi7()
