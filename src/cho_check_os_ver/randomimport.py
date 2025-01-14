1f90a9733b141216b4b1a8f4b15534c9bf1070e8
cho6019 <j01040864205@gmail.com>
2025-01-14 11:12:55 +0900

    random pick added

--- src/cho_check_os_ver/randomimport.py
+++ src/cho_check_os_ver/randomimport.py
@@ -7,22 +7,21 @@ from stundrg_check_os_ver.hi import hi as hi5
 from nunininu_check_os_ver.hi import hi as hi6
 #from seo-check-os-version.hi import hi as hi7
 
+def pick():
+    a = random.randint(1, 7)
 
-a = random.randint(1, 8)
-
-if a==1:
-    A=hi1()
-elif a==2:
-    A=hi2()
-elif a==3:
-    A=hi3()
-elif a==4:
-    A=hi4()
-elif a==5:
-    A=hi5()
-elif a==6:
-    A=hi6()
-#elif a==7:
-    #A=hi7()
-
-print(A)
+    if a==1:
+        A=hi1()
+    elif a==2:
+        A=hi2()
+    elif a==3:
+        A=hi3()
+    elif a==4:
+        A=hi4()
+    elif a==5:
+        A=hi5()
+    elif a==6:
+        A=hi6()
+    #elif a==7:
+        #A=hi7()
+    return(A)
