61b02279689cea158a6c8867e7345644a9c72c80
cho6019 <j01040864205@gmail.com>
2025-01-14 15:02:34 +0900

    change code

--- src/cho_check_os_ver/randomimport.py
+++ src/cho_check_os_ver/randomimport.py
@@ -10,18 +10,25 @@ from nunininu_check_os_ver.hi import hi as hi6
 def pick():
     a = random.randint(1, 7)
 
-    if a==1:
-        A=hi1()
-    elif a==2:
-        A=hi2()
-    elif a==3:
-        A=hi3()
-    elif a==4:
-        A=hi4()
-    elif a==5:
-        A=hi5()
-    elif a==6:
-        A=hi6()
+
+    match i:
+        case 1:
+            from check_os_ver.hi import hi as hi1
+            return(hi1())
+        case 2:
+            from hj_check_os_version.hi import hi as hi2
+            return(hi2())
+        case 3:
+            from jacob_os_version_check.hi import hi as hi3
+            return(hi3())
+        case 4:
+            from lucas_check_os_ver.hi import hi as hi4
+            return(hi4())
+        case 5:
+            from stundrg_check_os_ver.hi import hi as hi5
+            return(hi5())
+        case 6:
+            from nunininu_check_os_ver.hi import hi as hi6
+            return(hi6())
     #elif a==7:
         #A=hi7()
-    return(A)
